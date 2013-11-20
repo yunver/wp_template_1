5cead141c6f548b0b0ee48f1cfb4f54e068cf610
Yuan Jing Zhou <yuan.jingzhou@finntack.com>
2013-11-20 15:44:09 +0800

    在首页添加静态精选滑块

---
+++ home.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+
+	<?php get_template_part( 'includes/featured', 'home' ); ?>
+
+<?php get_footer(); ?>
---
+++ includes/featured.php
@@ -0,0 +1,58 @@
+<div id="featured_section">
+	<div id="featured" class="flexslider">
+		<ul class="slides">
+			<li class="slide">
+				<a href="#">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/demoimg/s_1.jpg" alt="" width="960" height="360" />
+					<span class="overlay"></span>
+				</a>
+			</li>
+			<!-- end .slide -->
+			<li class="slide">
+				<a href="#">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/demoimg/s_3.jpg" alt="" width="960" height="360" />
+					<span class="overlay"></span>
+				</a>
+			</li>
+			<!-- end .slide -->
+			<li class="slide">
+				<a href="#">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/demoimg/s_2.jpg" alt="" width="960" height="360" />
+					<span class="overlay"></span>
+				</a>
+			</li>
+			<!-- end .slide -->
+		</ul>
+	</div>
+	<!-- end #featured -->
+	<div id="switcher-container">
+		<ul id="switcher" class="clearfix">
+			<li class="active-slide">
+				<div class="switcher-content">
+					<span class="post-meta">一月<span>05</span></span>
+					<h2>我的背包</h2>
+					<p class="meta-info">发表于文章</p>
+				</div>
+				<!--end .switcher-content-->
+			</li>
+			<li>
+				<div class="switcher-content">
+					<span class="post-meta">二月<span>04</span></span>
+					<h2>亲爱的，再等等</h2>
+					<p class="meta-info">发表于文章</p>
+				</div>
+				<!--end .switcher-content-->
+			</li>
+			<li class="last">
+				<div class="switcher-content">
+					<span class="post-meta">三月<span>05</span></span>
+					<h2>给迓舒的一封信</h2>
+					<p class="meta-info">发表于文章</p>
+				</div>
+				<!--end .switcher-content-->
+			</li>
+		</ul>
+	</div>
+	<!--end #switcher-container-->
+</div>
+<!--end #featured_section-->
